a54fda0ec47e40697c35661555446ec41355d2ca
Vernon Hezron <vernonhezron@gmail.com>
2022-06-24 21:50:40 +0300

    First port scan (unmodified)

--- Python2.py
+++ Python2.py
@@ -1,3 +1,40 @@
-import pyautogui
-pyautogui.screenshot()
+import socket
+import argparse
+import threading
 
+
+def connection_scan(target_ip, target_port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target_ip, target_port))
+        s.send(b'Banner query\r\n')
+        results = s.recv(100)
+        print("[+] {}/tcp open".format(target_port))
+        print("[+] {}".format(str((results))))
+    except OSError:
+        print("[-] {}/tcp closed".format(target_port))
+    finally:
+        s.close()
+    
+
+def port_scan(target, port_num):
+    try:
+        target_ip = socket.gethostbyname(target)
+    except OSError:
+        print("[^] Cannot rexolve {}".format(target))
+        return
+    
+    try:
+        target_name = socket.gethostbyaddr(target_ip)
+        print("[*] Scan results for {}".format(target_name[0]))
+    except OSError:
+        print("[*] Scan results for {}".format(target_ip))
+
+    t = threading.Thread(target=connection_scan, args=(target, int(port_num)))
+    t.start
+
+
+def argument_parser():
+    parser = argparse.ArgumentParser(description="TCP port scanner")
+    parser.add_argument("-o", "--host", nargs="?", help="Host IP Address")
+    parser.add_argument("-p", "--ports", nargs="?", help="Comma-separated port list")
