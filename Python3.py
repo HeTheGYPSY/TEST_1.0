ff3af12f1f765761f4ab16c3877bd2ab0c3d5b5f
Vernon Hezron <vernonhezron@gmail.com>
2022-06-25 13:51:57 +0300

    Auth defined on function

--- Python3.py
+++ Python3.py
@@ -1,16 +1,22 @@
-import google.auth
-from google.oauth2 import id_token
-from google.oauth2 import service_account
-import google.auth.transport.requests
-from google.auth.transport.requests import AuthorizedSession
-target_audience = 'https://your-cloud-run-app.a.run.app'
-url = 'https://your-cloud-run-app.a.run.app'
-creds = service_account.IDTokenCredentials.from_service_account_file('/path/to/svc.json', target_audience=target_audience)
-authed_session = AuthorizedSession(creds)
-resp = authed_session.get(url)
-print(resp.status_code)
-print(resp.text)
-request = google.auth.transport.requests.Request()
-token = creds.token
-print(token)
-print(id_token.verify_token(token, request))
+def auth1():
+    try:
+        import google.auth
+        from google.oauth2 import id_token
+        from google.oauth2 import service_account
+        import google.auth.transport.requests
+        from google.auth.transport.requests import AuthorizedSession
+        target_audience = 'https://your-cloud-run-app.a.run.app'
+        url = 'https://your-cloud-run-app.a.run.app'
+        creds = service_account.IDTokenCredentials.from_service_account_file('/path/to/svc.json', target_audience=target_audience)
+        authed_session = AuthorizedSession(creds)
+        resp = authed_session.get(url)
+        print(resp.status_code)
+        print(resp.text)
+        request = google.auth.transport.requests.Request()
+        token = creds.token
+        print(token)
+        print(id_token.verify_token(token, request))
+    except Exception as err_msg:
+        print(err_msg)
+    finally:
+        print("**Execution Complete!**")
