cfcdbdf1ab9cd7ba6363d3b371d7c77a489dfbc8
Vernon Hezron <vernonhezron@gmail.com>
2022-06-25 12:48:26 +0300

    Google Authenticator Trial #1

--- Python3.py
+++ Python3.py
@@ -1,4 +1,20 @@
-import socket
-x = socket.gethostname()
-print(socket.gethostbyname(x))
-print(x)
+import google.auth
+from google.oauth2 import id_token
+from google.oauth2 import service_account
+import google.auth
+import google.auth.transport.requests
+from google.auth.transport.requests import AuthorizedSession
+target_audience = 'https://your-cloud-run-app.a.run.app'
+url = 'https://your-cloud-run-app.a.run.app'
+creds = service_account.IDTokenCredentials.from_service_account_file(
+'/path/to/svc.json', target_audience=target_audience)
+authed_session = AuthorizedSession(creds)
+# make authenticated request and print the response, status_code
+resp = authed_session.get(url)
+print(resp.status_code)
+print(resp.text)
+# to verify an ID Token
+request = google.auth.transport.requests.Request()
+token = creds.token
+print(token)
+print(id_token.verify_token(token,request))
