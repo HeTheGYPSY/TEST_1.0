735a227909f28ce6ce545948b0f192a19368863a
Vernon Hezron <vernonhezron@gmail.com>
2022-07-08 09:31:08 +0300

    OS info extraction.

--- Python6.py
+++ Python6.py
@@ -57,20 +57,6 @@ def GPU_info():
                                        "temperature", "uuid")))
 
 
-def get_size(bytes, suffix="B"):
-    """
-    Scale bytes to its proper format
-    e.g:
-        1253656 => '1.20MB'
-        1253656678 => '1.17GB'
-    """
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
-
-
 def sys_info():
     print("=" * 40, "System Information", "=" * 40)
     uname = platform.uname()
