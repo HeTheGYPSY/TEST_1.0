f256664e5faad9be8388df01a42a6be4df81f16b
Vernon Hezron <vernonhezron@gmail.com>
2022-07-08 08:10:12 +0300

    OS info extraction

--- Python6.py
+++ Python6.py
@@ -1,44 +1,159 @@
+import platform
+import GPUtil
 import hashlib
+import psutil
+import datetime
 
-print("**************PASSWORD CRACKER ******************")
 
-# To check if the password
-# found or not.
-pass_found = 0
+def pass_crack():
+    print("**************PASSWORD CRACKER ******************")
+    pass_found = 0  # To check if the password is found or not.
+    input_hash = input("Enter the hashed password:")
+    pass_doc = input("\nEnter passwords filename including path(root / home/):")
+    try:
+        pass_file = open(pass_doc, 'r')      # trying to open the password file.
+    except OSError:
+        print("Error:")
+        print(pass_doc, "is not found.\nPlease give the path of file correctly.")
 
-input_hash = input("Enter the hashed password:")
+        # comparing the input_hash with the hashes of the words in password file and finding password.
+    for word in pass_file:
+        enc_word = word.encode('utf-8')      # encoding the word into utf-8 format
+        hash_word = hashlib.md5(enc_word.strip())    # Hashing a word into md5 hash
+        digest = hash_word.hexdigest()    # digesting that hash into a hexadecimal value
 
-pass_doc = input("\nEnter passwords filename including path(root / home/):")
+        if digest == input_hash:
+            # comparing hashes
+            print("Password found.\nThe password is:", word)
+            pass_found = 1
+            break
 
-try:
-    # trying to open the password file.
-    pass_file = open(pass_doc, 'r')
-except OSError:
-    print("Error:")
-    print(pass_doc, "is not found.\nPlease give the path of file correctly.")
+    if not pass_found:
+        print("Password is not found in the", pass_doc, "file")
+        print('\n')
+    print("***************** Thank you **********************")
 
-# comparing the input_hash with the hashes
-# of the words in password file,
-# and finding password.
 
-for word in pass_file:
-    # encoding the word into utf-8 format
-    enc_word = word.encode('utf-8')
+def GPU_info():
+    from tabulate import tabulate
+    print("=" * 40, "GPU Details", "=" * 40)
+    gpus = GPUtil.getGPUs()
+    list_gpus = []
+    for gpu in gpus:
+        gpu_id = gpu.id
+        gpu_name = gpu.name
+        gpu_load = f"{gpu.load * 100}%"
+        gpu_free_memory = f"{gpu.memoryFree}MB"
+        gpu_used_memory = f"{gpu.memoryUsed}MB"
+        gpu_total_memory = f"{gpu.memoryTotal}MB"
+        gpu_temperature = f"{gpu.temperature} °C"
+        gpu_uuid = gpu.uuid
+        list_gpus.append((
+            gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
+            gpu_total_memory, gpu_temperature, gpu_uuid
+        ))
 
-    # Hashing a word into md5 hash
-    hash_word = hashlib.md5(enc_word.strip())
+    print(tabulate(list_gpus, headers=("id", "name", "load", "free memory", "used memory", "total memory",
+                                       "temperature", "uuid")))
 
-    # digesting that hash into a hexadecimal value
-    digest = hash_word.hexdigest()
 
-    if digest == input_hash:
-        # comparing hashes
-        print("Password found.\nThe password is:", word)
-        pass_found = 1
-        break
+def get_size(bytes, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
 
-# if password is not found.
-if not pass_found:
-    print("Password is not found in the", pass_doc, "file")
-    print('\n')
-print("***************** Thank you **********************")
+
+def sys_info():
+    print("=" * 40, "System Information", "=" * 40)
+    uname = platform.uname()
+    print(f"System: {uname.system}")
+    print(f"Node Name: {uname.node}")
+    print(f"Release: {uname.release}")
+    print(f"Version: {uname.version}")
+    print(f"Machine: {uname.machine}")
+    print(f"Processor: {uname.processor}")
+    # Boot Time
+    print("=" * 40, "Boot Time", "=" * 40)
+    boot_time_timestamp = psutil.boot_time()
+    bt = datetime.fromtimestamp(boot_time_timestamp)
+    print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
+
+
+def CPU_info():
+    print("=" * 40, "CPU Info", "=" * 40)
+    print("Physical cores:", psutil.cpu_count(logical=False))
+    print("Total cores:", psutil.cpu_count(logical=True))
+    cpufreq = psutil.cpu_freq()
+    print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+    print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+    print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+    print("CPU Usage Per Core:")
+    for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+        print(f"Core {i}: {percentage}%")
+    print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+
+
+def mem_info():
+    # Memory Information
+    print("=" * 40, "Memory Information", "=" * 40)
+    # get the memory details
+    svmem = psutil.virtual_memory()
+    print(f"Total: {get_size(svmem.total)}")
+    print(f"Available: {get_size(svmem.available)}")
+    print(f"Used: {get_size(svmem.used)}")
+    print(f"Percentage: {svmem.percent}%")
+    print("=" * 20, "SWAP", "=" * 20)
+    swap = psutil.swap_memory()  # get the swap memory details (if exists)
+    print(f"Total: {get_size(swap.total)}")
+    print(f"Free: {get_size(swap.free)}")
+    print(f"Used: {get_size(swap.used)}")
+    print(f"Percentage: {swap.percent}%")
+
+
+def disk_info():
+    print("=" * 40, "Disk Information", "=" * 40)
+    print("Partitions and Usage:")
+    partitions = psutil.disk_partitions()  # get all disk partitions
+    for partition in partitions:
+        print(f"=== Device: {partition.device} ===")
+        print(f"  Mountpoint: {partition.mountpoint}")
+        print(f"  File system type: {partition.fstype}")
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+        except PermissionError:
+            # this can be cached due to the disk that isn't ready
+            continue
+        print(f"  Total Size: {get_size(partition_usage.total)}")
+        print(f"  Used: {get_size(partition_usage.used)}")
+        print(f"  Free: {get_size(partition_usage.free)}")
+        print(f"  Percentage: {partition_usage.percent}%")
+    disk_io = psutil.disk_io_counters()  # get IO statistics since boot
+    print(f"Total read: {get_size(disk_io.read_bytes)}")
+    print(f"Total write: {get_size(disk_io.write_bytes)}")
+
+
+def net_info():
+    print("=" * 40, "Network Information", "=" * 40)
+    if_addrs = psutil.net_if_addrs()  # get all network interfaces (virtual and physical)
+    for interface_name, interface_addresses in if_addrs.items():
+        for address in interface_addresses:
+            print(f"=== Interface: {interface_name} ===")
+            if str(address.family) == 'AddressFamily.AF_INET':
+                print(f"  IP Address: {address.address}")
+                print(f"  Netmask: {address.netmask}")
+                print(f"  Broadcast IP: {address.broadcast}")
+            elif str(address.family) == 'AddressFamily.AF_PACKET':
+                print(f"  MAC Address: {address.address}")
+                print(f"  Netmask: {address.netmask}")
+                print(f"  Broadcast MAC: {address.broadcast}")
+    net_io = psutil.net_io_counters()  # get IO statistics since boot
+    print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
+    print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
