f0d5973df535c088b1e016e411fc979850d8f6e0
VERNON HEZRON <103187250+HeTheGYPSY@users.noreply.github.com>
2022-07-10 13:04:17 +0300

    Typo fix 1.2

--- Python1.py
+++ Python1.py
@@ -1,17 +1,17 @@
 import hashlib
 import os
 print("**************PASSWORD CRACKER ******************")
-pass_found = 0 # To check if the password  found or not
+pass_found = 0  # To check if the password  found or not
 input_hash = input("Enter the hashed password:")
 pass_doc = input("\nEnter passwords filename including path(root / home/): ")
-if os.access(pass_doc, os.R_OK) and os.access(pass_doc, os.W_OK) == True:
+if os.access(pass_doc, os.R_OK) is True:
     try:
         pass_file = open(pass_doc, 'r')
 
         for word in pass_file:  # comparing the input_hash with the hashes of the words in password file
             enc_word = word.encode('utf-8')
             hash_word = hashlib.md5(enc_word.strip())
-            digest = hash_word.hexdigest()  # digesting that hash into a hexa decimal value
+            digest = hash_word.hexdigest()  # digesting that hash into a hexadecimal value
             if digest == input_hash:
                 print("Password found.\nThe password is:", word)  # comparing hashes
                 pass_found = 1
